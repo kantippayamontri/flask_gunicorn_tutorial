e0a0b9078e004b587b4a29c409792a2e038f4cb6
Kan Tippayamontri <kantip@MacBook-Pro-khxngKan-2.local>
2023-07-25 09:05:53 +0700

    first commit

---
+++ peak.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # return 'Hello World!'
+    # return render_template("index.html", mountain="Everest")
+    
+    mountains = ["Everest", "K2", "Kilimanjaro"]
+    return render_template("index.html", mountains=mountains)
+
+@app.route('/mountain/<mt>')
+def mountain(mt):
+    return "This is " + str(mt)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=2000,debug=True)
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from peak import app
+
+if __name__ == '__main__':
+    app.run() # don't need to use port and host -> dev environment
+                # production environment we just app.run()
